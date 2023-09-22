d8f79ce997e89d99a564dd36fca4b60227a3a8d9
Rahul <rahul.v@mobitrail.com>
2023-09-22 14:52:28 +0530

    Miracle happend

--- MVVM_RND/MVVM/ViewController.swift
+++ MVVM_RND/MVVM/ViewController.swift
@@ -70,7 +70,7 @@ class ViewController: UIViewController {
         dataSource = TablesDataSource(cellIdentifier: "tableCartCell", items: viewModel.dummyCartData.carts[0].products, configureCell: { cell, product in
 
             cell.labelTitle.text = product.title
-            cell.labelDesc.text = "Price: \(product.price)  Disw Price: \(product.discountedPrice)"
+            cell.labelDesc.text = "PriceABVC: \(product.price)  Disw Price: \(product.discountedPrice)"
 
         })
 
